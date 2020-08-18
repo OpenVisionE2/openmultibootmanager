1a3f9d6b36d8c48451d71bf2ead20bd786b8c836
Persian Prince <persianpros@yahoo.com>
2020-08-18 22:59:49 +0430

    Open Vision ready

--- po/updateallpo.sh
+++ po/updateallpo.sh
@@ -3,19 +3,19 @@
 #  
 # Pre-requisite:
 # The following tools must be installed on your system and accessible from path
-# gawk, find, xgettext, gsed, python, msguniq, msgmerge, msgattrib, msgfmt, msginit
+# gawk, find, xgettext, sed, python, msguniq, msgmerge, msgattrib, msgfmt, msginit
 #
 # Run this script from within the po folder.
 #
 # Author: Pr2
 # Version: 1.2
-PluginName=open-multiboot
+PluginName=OpenMultiboot
 printf "Po files update/creation from script starting.\n"
 #
 # Update Makefile.am to include all existing language files sorted
 #
 makelanguages=$(ls *.po | tr "\n" " " | sed 's/.po//g')
-gsed -i 's/LANGS .*/LANGS = '"$makelanguages"'/' Makefile.am
+sed -i 's/LANGS .*/LANGS = '"$makelanguages"'/' Makefile.am
 #
 # Retrieve languages from Makefile.am LANGS variable for backward compatibility
 #
@@ -27,7 +27,7 @@ languages=($(gawk ' BEGIN { FS=" " }
 #
 # To use the existing files as reference for languages
 #
-# languages=($(ls *.po | gsed 's/\.po//'))		
+# languages=($(ls *.po | sed 's/\.po//'))		
 
 # If you want to define the language locally in this script uncomment and defined languages
 #languages=("ar" "bg" "ca" "cs" "da" "de" "el" "en" "es" "et" "fa" "fi" "fr" "fy" "he" "hk" "hr" "hu" "id" "is" "it" "ku" "lt" "lv" "nl" "nb" "nn" "pl" "pt" "pt_BR" "ro" "ru" "sk" "sl" "sr" "sv" "th" "tr" "uk" "zh")
@@ -39,7 +39,7 @@ languages=($(gawk ' BEGIN { FS=" " }
 
 printf "Creating temporary file $PluginName-py.pot\n"
 find -s -X .. -name "*.py" -exec xgettext --no-wrap -L Python --from-code=UTF-8 -kpgettext:1c,2 --add-comments="TRANSLATORS:" -d $PluginName -s -o $PluginName-py.pot {} \+
-gsed --in-place $PluginName-py.pot --expression=s/CHARSET/UTF-8/
+sed --in-place $PluginName-py.pot --expression=s/CHARSET/UTF-8/
 printf "Creating temporary file $PluginName-xml.pot\n"
 find -s -X .. -name "*.xml" -exec python xml2po.py {} \+ > $PluginName-xml.pot
 printf "Merging pot files to create: $PluginName.pot\n"
@@ -61,5 +61,3 @@ done
 rm $PluginName-py.pot $PluginName-xml.pot
 IFS=$OLDIFS
 printf "Po files update/creation from script finished!\n"
-
-
